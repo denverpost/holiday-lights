c46255ffa268f4b2ae48f6b84912dde5f930ef14
githamm <hamm.kevin@gmail.com>
2015-11-22 23:32:56 -0700

    Working with geojson

---
+++ test-geojson.js
@@ -0,0 +1,33 @@
+{
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -104.951339,39.72527 ]
+    },
+    "properties": {
+    "title":"Mizell's Christmas Home",
+    "address":"8755 W. 91st Place",
+    "city":"Broomfield",
+    "ZIP code":"80021",
+    "photo":"test-image.jpg"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -104.951339,39.82527 ]
+    },
+    "properties": {
+    "title":"Grinch's Christmas Home",
+    "address":"8755 W. 91st Place",
+    "city":"Broomfield",
+    "ZIP code":"80021",
+    "photo":"test-image.jpg"
+    }
+  }
+  ]
+}
